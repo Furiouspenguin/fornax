067e70ef71ad32cf55b500b73b765180a4d84db0
Furiouspenguin <47417798+Furiouspenguin@users.noreply.github.com>
2025-05-24 22:25:18 +0000

    [frontend] toggleable device updates

--- frontend/src/app/components/devices/devices.component.ts
+++ frontend/src/app/components/devices/devices.component.ts
@@ -2,22 +2,25 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button'
 import { TableModule } from 'primeng/table'
 import { ChartModule } from 'primeng/chart';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
 import { Device } from '../../models/device.model';
 import { DeviceService } from '../../services/device.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-devices',
-  imports: [ButtonModule, TableModule, ChartModule, CommonModule],
+  imports: [ButtonModule, TableModule, ChartModule, ToggleSwitchModule, CommonModule, FormsModule],
   templateUrl: './devices.component.html',
   styleUrl: './devices.component.scss',
 })
 export class DevicesComponent implements OnInit, OnDestroy {
   
   devices: Device[] = [];
-  interval ?: NodeJS.Timeout = undefined
-  options: any
+  interval ?: NodeJS.Timeout = undefined;
+  options: any;
+  isUpdatingChart = true;
 
   constructor(protected deviceService: DeviceService) {
     this.initChartOptions();
@@ -64,8 +67,10 @@ export class DevicesComponent implements OnInit, OnDestroy {
   
   
   ngOnInit(): void {
-    this.deviceService.getDevicesData()
-    this.interval = setInterval(this.deviceService.getDevicesData, 4000)
+    this.deviceService.getDevicesData();
+    this.interval = setInterval(() => {
+      if (this.isUpdatingChart) this.deviceService.getDevicesData();
+    }, 4000);
   }
 
 
